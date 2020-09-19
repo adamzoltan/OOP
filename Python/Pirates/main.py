722c9e85d70f62d4cb7a55324c2c1101efde1a01
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-09-19 12:57:00 +0200

    create how is it going function

--- Python/Pirates/main.py
+++ Python/Pirates/main.py
@@ -4,3 +4,5 @@ if __name__ == "__main__":
     
     prirate_1 = Pirate()
     prirate_1.drink_some_rum()
+    prirate_1.hows_it_going_mate()
+    
--- Python/Pirates/pirate.py
+++ Python/Pirates/pirate.py
@@ -5,3 +5,10 @@ class Pirate:
     def drink_some_rum(self):
         self.intoxication_level += 1
         print(f"The pirate drank some rum! His intoxication level is {self.intoxication_level}.")
+
+    def hows_it_going_mate(self):
+        if self.intoxication_level <= 4:
+            print("Pour me anudder!")
+        else:
+            print("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
+            self.intoxication_level = 0
