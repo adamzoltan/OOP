b879b90853976e8d7e8a7f2950d38bddccf33c1b
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-09-19 14:34:43 +0200

    fix pirate init in main

--- Python/Pirates/main.py
+++ Python/Pirates/main.py
@@ -2,10 +2,10 @@ from pirate import Pirate
 
 if __name__ == "__main__":
     
-    prirate_1 = Pirate()
+    prirate_1 = Pirate("Jack")
     prirate_1.drink_some_rum()
     prirate_1.hows_it_going_mate()
     
-    prirate_2 = Pirate()
+    prirate_2 = Pirate("David")
 
     prirate_1.brawl(prirate_2)
