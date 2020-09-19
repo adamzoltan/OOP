35be847496bf56f5d182d0d04515e726153bbad6
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-09-19 12:26:33 +0200

    create drink some rum function

---
+++ Python/Pirates/pirate.py
@@ -0,0 +1,7 @@
+class Pirate
+    
+    intoxication_level = 0
+
+    def drink_some_rum(self):
+        self.intoxication_level += 1
+        print(f"The pirate drank some rum! His intoxication level is {intoxication_level}")
