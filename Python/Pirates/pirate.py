fb7526099b3fb46d399edfecce640c7a3b09c37d
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-09-19 13:00:18 +0200

    create die function

--- Python/Pirates/pirate.py
+++ Python/Pirates/pirate.py
@@ -1,14 +1,24 @@
 class Pirate:
     
+    is_alive = True
     intoxication_level = 0
 
     def drink_some_rum(self):
-        self.intoxication_level += 1
-        print(f"The pirate drank some rum! His intoxication level is {self.intoxication_level}.")
+        if self.is_alive:
+            self.intoxication_level += 1
+            print(f"The pirate drank some rum! His intoxication level is {self.intoxication_level}.")
+        else:
+            print("He is dead...")
 
     def hows_it_going_mate(self):
-        if self.intoxication_level <= 4:
-            print("Pour me anudder!")
+        if self.is_alive:
+            if self.intoxication_level <= 4:
+                print("Pour me anudder!")
+            else:
+                print("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
+                self.intoxication_level = 0
         else:
-            print("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
-            self.intoxication_level = 0
+            print("He is dead...")
+
+    def die(self):
+        self.is_alive = False
