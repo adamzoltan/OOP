6d69643c1b030f42b65d3c468122cbd7d3d9d83b
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-09-19 12:49:11 +0200

    create main method and a pirate

---
+++ Python/Pirates/main.py
@@ -0,0 +1,6 @@
+from pirate import Pirate
+
+if __name__ == "__main__":
+    
+    prirate_1 = Pirate()
+    prirate_1.drink_some_rum()
--- Python/Pirates/pirate.py
+++ Python/Pirates/pirate.py
@@ -1,7 +1,7 @@
-class Pirate
+class Pirate:
     
     intoxication_level = 0
 
     def drink_some_rum(self):
         self.intoxication_level += 1
-        print(f"The pirate drank some rum! His intoxication level is {intoxication_level}")
+        print(f"The pirate drank some rum! His intoxication level is {self.intoxication_level}.")
