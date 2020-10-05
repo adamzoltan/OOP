51bff161b0ea15849222a0ccd4856af17970acea
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-10-05 15:13:36 +0200

    create refill method for Aircraft

--- Python/AircraftCarrier/aircraft.py
+++ Python/AircraftCarrier/aircraft.py
@@ -9,3 +9,15 @@ class Aircraft():
         dealt_damage = self.current_ammo * self.base_damage
         self.current_ammo = 0
         return(dealt_damage)
+    
+    def refill(self, ammo):
+        needed_ammo = self.max_ammo - self.current_ammo
+        if ammo >= needed_ammo:
+            self.current_ammo = self.current_ammo + needed_ammo
+            return(ammo - needed_ammo)
+        else:
+            self.current_ammo = self.current_ammo + ammo
+            return(0)
+
+
+
